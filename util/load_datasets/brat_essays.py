3bb0410e2b498501686b6898725e59d90b11625f
NadjaNauta12 <crazy.time@yahoo.de>
2019-11-06 17:18:57 +0100

    Loading Annotation and Brat Essays working

--- util/load_datasets/brat_essays.py
+++ util/load_datasets/brat_essays.py
@@ -1,5 +1,6 @@
 import os
 import codecs
+import pandas as pd
 
 def parse_essays(path):
     essays = []
@@ -7,15 +8,24 @@ def parse_essays(path):
         for file in files:
             if '.txt' in file:
                 with codecs.open(os.path.join(subdir, file), mode="r", encoding="utf8") as essay_file:
-                    essays.append(Essay(file, essay_file))
+                    content = essay_file.read()
+                    essays.append(Essay(int(file[5:8]), content))
 
     return essays
 
 class Essay():
-    def __init__(self, file,  text=""):
+    def __init__(self, essay,  text=""):
         # assign id
-        self.file = file
+        self.essay = essay
         self.text = text
+
+    def as_dict(self):
+        return {'file': self.essay, 'text': self.text}
+
+
+
 
 
 essays = parse_essays(r"C:\Users\Wifo\PycharmProjects\Masterthesis\data\Argument_Component_Identification_Habernal\brat-project-final")
+df_essay = pd.DataFrame.from_records([essay.as_dict() for essay in essays])
+print(df_essay.head())
